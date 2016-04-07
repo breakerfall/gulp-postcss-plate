3c68091d57d2d33c97e517adb81e7a7dd1e927e0
natpromo <sean@web>
2016-04-07 11:13:16 +1000

    move postcss processors up

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,15 @@ var gulp = require('gulp');
 gulp.task('css', function() {
   var postcss = require('gulp-postcss');
   var sourcemaps = require('gulp-sourcemaps');
+  var autoprefixer = require('autoprefixer');
+
+  var processors = [
+    autoprefixer
+  ];
 
   return gulp.src('src/**/*.css')
     .pipe(sourcemaps.init())
-    .pipe(postcss([ require('autoprefixer') ]))
+    .pipe(postcss(processors))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build/'));
 });
