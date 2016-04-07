672b3b19f02c112148659aac2eab33566a7fb0c8
natpromo <sean@web>
2016-04-07 10:53:28 +1000

    setup basic postcss workflow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 
+gulp.task('css', function() {
+  var postcss = require('gulp-postcss');
+  var sourcemaps = require('gulp-sourcemaps');
+
+  return gulp.src('src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ require('autoprefixer') ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/'));
+});
+
 gulp.task('default', function() {
 
 });
